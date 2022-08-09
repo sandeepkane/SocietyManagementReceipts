70f396e1e5dc10240ad4b7fad7b7a7b7788f5b05
SandeepKane-at-587358001311 <sandeep.kane@wipro.com>
2022-08-09 18:00:34 +0530

    Code changed and added dynamic names to file

--- src/main/java/com/example/utils/AmountTranslate.java
+++ src/main/java/com/example/utils/AmountTranslate.java
@@ -1,38 +1,40 @@
 package com.example.utils;
 
-import java.util.Locale;
-
-import org.springframework.util.StringUtils;
-
 import com.ibm.icu.math.BigDecimal;
 import com.ibm.icu.text.RuleBasedNumberFormat;
 import com.ibm.icu.util.Currency;
 import com.ibm.icu.util.CurrencyAmount;
+import org.springframework.util.StringUtils;
+
+import java.util.Locale;
 
 public class AmountTranslate {
 
-	static String COUNTRY_CODE = "IN";
-	static String LANG = "en";
-	static String FRACTION_UNIT_NAME = "Rupees";
+    static String COUNTRY_CODE = "IN";
+    static String LANG = "en";
+    static String FRACTION_UNIT_NAME = "Rupees";
 
-	public static String translate(String amount) {
-		StringBuffer result = new StringBuffer();
+    public static String translate(String amount) {
+        StringBuilder result = new StringBuilder();
 
-		Locale locale = new Locale(LANG, COUNTRY_CODE);
-		Currency crncy = Currency.getInstance(locale);
+        Locale locale = new Locale(LANG, COUNTRY_CODE);
+        Currency crncy = Currency.getInstance(locale);
 
-		String inputArr[] = StringUtils.split(new BigDecimal(amount).abs().toString(), ".");
-		RuleBasedNumberFormat rule = new RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT);
+        String[] inputArr = StringUtils.split(new BigDecimal(amount).abs().toString(), ".");
+        RuleBasedNumberFormat rule = new RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT);
 
-		int i = 0;
-		for (String input : inputArr) {
-			CurrencyAmount crncyAmt = new CurrencyAmount(new BigDecimal(input), crncy);
-			if (i++ == 0) {
-				result.append(rule.format(crncyAmt)).append(" " + crncy.getDisplayName() + " and ");
-			} else {
-				result.append(rule.format(crncyAmt)).append(" " + FRACTION_UNIT_NAME + " ");
-			}
-		}
-		return result.toString();
-	}
+        int i = 0;
+        if (inputArr == null) {
+            return "ERROR";
+        }
+        for (String input : inputArr) {
+            CurrencyAmount crncyAmt = new CurrencyAmount(new BigDecimal(input), crncy);
+            if (i++ == 0) {
+                result.append(rule.format(crncyAmt)).append(" ").append(crncy.getDisplayName()).append(" and ");
+            } else {
+                result.append(rule.format(crncyAmt)).append(" ").append(FRACTION_UNIT_NAME).append(" ");
+            }
+        }
+        return result.toString();
+    }
 }
